214378e9c452ac8b83f8f9aad15f71845a0c2472
zzppvvss <84504709+zzppvvss@users.noreply.github.com>
2023-05-23 01:57:56 +0300

    Add files via upload

---
+++ SoftwareEnVetProject-master/SoftwareEnVetProject/log_in.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SoftwareEnVetProject
+{
+    public partial class log_in : Form
+    {
+        public log_in()
+        {
+            InitializeComponent();
+        }
+
+        private void EnterForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sign_up new_Form = new sign_up();
+            new_Form.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void splitter1_SplitterMoved(object sender, SplitterEventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ SoftwareEnVetProject-master/SoftwareEnVetProject/sign_up.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SoftwareEnVetProject
+{
+    public partial class sign_up : Form
+    {
+        public sign_up()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            sign_up_owner form1 = new sign_up_owner();
+            form1.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            sign_up_doctor form1 = new sign_up_doctor();
+            form1.Show();
+        }
+    }
+}
